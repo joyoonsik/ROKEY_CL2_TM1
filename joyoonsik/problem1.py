41ace7d16dbcf9f08c376a4ed356a92b96db8b35
joyoonsik <ysixjo@gmail.com>
2024-09-30 12:11:17 +0900

    problem 1 solve

---
+++ joyoonsik/problem1.py
@@ -0,0 +1,35 @@
+#문제요약
+#선물지수:준선물-받은선물
+#선물지수가 더 높은사람이 선물받음
+#선물지수가 같다면 선물 주고받지 않음
+#다음달 가장 많이 선물받는자의 선물 수를 예측하라
+def solution(friends, gifts):
+    #준사람,받은사람
+    dic = {i:{j:0 for j in friends} for i in friends}
+    
+    #받을선물,선물지수
+    dic_gift = {i:[0,0] for i in friends}
+    
+    for i in gifts:
+        dic[i.split(" ")[0]][i.split(" ")[1]]+=1
+        dic_gift[i.split(" ")[0]][1]+=1
+        dic_gift[i.split(" ")[1]][1]+=-1
+
+    for i,v in enumerate(friends):
+        for j in friends[i+1:]:
+            if dic[v][j] < dic[j][v]:
+                dic_gift[j][0]+=1
+            elif dic[v][j] > dic[j][v]:
+                dic_gift[v][0]+=1
+            else:
+                if dic_gift[j][1] < dic_gift[v][1]:
+                    dic_gift[v][0]+=1
+                elif dic_gift[j][1] > dic_gift[v][1]:
+                    dic_gift[j][0]+=1     
+    
+    return max([i[0] for i in dic_gift.values()])
+    
+
+#해결포인트
+#1.먼저 딕셔너리의 키값을 friends값으로 할당시킨것
+#2.문제에서 제시한 표를 순서대로 구현
